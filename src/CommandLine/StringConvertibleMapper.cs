56756270e3bc2d3dd08c1830fcb1d66fe32dee9f
JeremyTCD <jeremytanchongdao@gmail.com>
2017-09-01 22:20:36 +0800

    Added StringConvertibleMapper

---
+++ src/CommandLine/StringConvertibleMapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+namespace JeremyTCD.DotNet.CommandLine
+{
+    public class StringConvertibleMapper : IMapper
+    {
+        /// <summary>
+        /// If <paramref name="value"/> is not null and the property represented by <paramref name="propertyInfo"/> is of a type that 
+        /// can be converted to from <see cref="string"/>, sets corresponding property in <paramref name="target"/> to <paramref name="value"/>.
+        /// </summary>
+        /// <param name="propertyInfo"></param>
+        /// <param name="value"></param>
+        /// <param name="target"></param>
+        /// <returns>
+        /// True if property was set succesfully, false otherwise.
+        /// </returns>
+        public bool TryMap(PropertyInfo propertyInfo, string value, object target)
+        {
+            if(value == null || !CanBeConvertedToFromString(propertyInfo.PropertyType))
+            {
+                return false;
+            }
+
+            propertyInfo.SetValue(target, Convert.ChangeType(value, propertyInfo.PropertyType));
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if <paramref name="type"/> can be converted to from <see cref="string"/>. All types that <see cref="string"/> has
+        /// an <see cref="IConvertible"/> member implementation for are included 
+        /// (https://github.com/dotnet/coreclr/blob/master/src/mscorlib/src/System/String.cs).
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns>
+        /// True if <paramref name="type"/> can be converted to from <see cref="string"/>.
+        /// </returns>
+        internal bool CanBeConvertedToFromString(Type type)
+        {
+            return type.GetTypeInfo().IsPrimitive || type == typeof(string) || type == typeof(DateTime) || type == typeof(decimal);
+        }
+    }
+}
---
+++ test/CommandLine/StringConvertibleMapperIntegrationTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace JeremyTCD.DotNet.CommandLine.Tests
+{
+    public class StringConvertibleMapperIntegrationTests
+    {
+        [Theory]
+        [MemberData(nameof(ReturnsTrueIfTypeCanBeConvertedToFromString))]
+        public void IsBuiltInType_ReturnsTrueIfTypeCanBeConvertedToFromString(Type type)
+        {
+            // Arrange
+            StringConvertibleMapper builtInTypeMapper = new StringConvertibleMapper();
+
+            // Act
+            bool result = builtInTypeMapper.CanBeConvertedToFromString(type);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        public static IEnumerable<object[]> ReturnsTrueIfTypeCanBeConvertedToFromString()
+        {
+            yield return new object[] { typeof(bool) };
+            yield return new object[] { typeof(byte) };
+            yield return new object[] { typeof(sbyte) };
+            yield return new object[] { typeof(short) };
+            yield return new object[] { typeof(char) };
+            yield return new object[] { typeof(double) };
+            yield return new object[] { typeof(float) };
+            yield return new object[] { typeof(int) };
+            yield return new object[] { typeof(uint) };
+            yield return new object[] { typeof(long) };
+            yield return new object[] { typeof(ulong) };
+            yield return new object[] { typeof(ushort) };
+            yield return new object[] { typeof(string) };
+            yield return new object[] { typeof(DateTime) };
+            yield return new object[] { typeof(decimal) };
+        }
+
+        [Theory]
+        [MemberData(nameof(ReturnsFalseIfTypeCannotBeConvertedToFromString))]
+        public void IsBuiltInType_ReturnsFalseIfTypeCannotBeConvertedToFromString(Type type)
+        {
+            // Arrange
+            StringConvertibleMapper builtInTypeMapper = new StringConvertibleMapper();
+
+            // Act
+            bool result = builtInTypeMapper.CanBeConvertedToFromString(type);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        public static IEnumerable<object[]> ReturnsFalseIfTypeCannotBeConvertedToFromString()
+        {
+            yield return new object[] { typeof(List<>) };
+            yield return new object[] { typeof(Array) };
+            yield return new object[] { typeof(DummyModel) };
+        }
+
+        [Fact]
+        public void TryMap_ReturnsFalseIfValueIsNull()
+        {
+            // Arrange
+            StringConvertibleMapper defaultMapper = new StringConvertibleMapper();
+
+            // Act
+            bool result = defaultMapper.TryMap(null, null, null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TryMap_ReturnsFalseIfPropertyTypeCannotBeConvertedToFromString()
+        {
+            // Arrange
+            PropertyInfo propertyInfo = typeof(DummyModel).GetProperty(nameof(DummyModel.NotConvertible));
+            StringConvertibleMapper defaultMapper = new StringConvertibleMapper();
+
+            // Act
+            bool result = defaultMapper.TryMap(propertyInfo, "dummy", null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TryMap_ReturnsTrueIfMappingIsSuccessful()
+        {
+            // Arrange
+            PropertyInfo propertyInfo = typeof(DummyModel).GetProperty(nameof(DummyModel.Convertible));
+            DummyModel dummyModel = new DummyModel();
+            StringConvertibleMapper defaultMapper = new StringConvertibleMapper();
+            string dummyString = "1";
+
+            // Act
+            bool result = defaultMapper.TryMap(propertyInfo, dummyString, dummyModel);
+
+            // Assert
+            Assert.Equal(1, dummyModel.Convertible);
+            Assert.True(result);
+        }
+
+        private class DummyModel
+        {
+            public List<int> NotConvertible { get; set; }
+            public int Convertible { get; set; }
+        }
+    }
+}
