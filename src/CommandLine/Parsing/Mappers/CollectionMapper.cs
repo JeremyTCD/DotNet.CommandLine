765f1838ae2699dc3cbc048a74e3299b15a19f06
JeremyTCD <jeremytanchongdao@gmail.com>
2017-09-01 21:26:04 +0800

    Added CollectionMapper

---
+++ src/CommandLine/Parsing/Mappers/CollectionMapper.cs
@@ -0,0 +1,67 @@
+﻿using JeremyTCD.DotNetCore.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace JeremyTCD.DotNet.CommandLine
+{
+    public class CollectionMapper : IMapper
+    {
+        private IActivatorService _activatorService { get; }
+
+        public CollectionMapper(IActivatorService activatorService)
+        {
+            _activatorService = activatorService;
+        }
+
+        /// <summary>
+        /// Ensures that <paramref name="value"/> is not null and that the property represented by <paramref name="propertyInfo"/> is assignable to
+        /// <see cref="ICollection{T}"/>. If both conditions are met, sets the property in 
+        /// <paramref name="target"/> to an <see cref="ICollection{T}"/> containing the result of splitting <paramref name="value"/> using ','
+        /// as separator.
+        /// </summary>
+        /// <param name="propertyInfo"></param>
+        /// <param name="value"></param>
+        /// <param name="target"></param>
+        /// <returns>
+        /// True if property was set succesfully, false otherwise.
+        /// </returns>
+        /// <exception cref="InvalidCastException"></exception>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="OverflowException"></exception>
+        public bool TryMap(PropertyInfo propertyInfo, string value, object target)
+        {
+            if(value == null)
+            {
+                return false;
+            }
+
+            IEnumerable<Type> collectionTypes = propertyInfo.
+                PropertyType.
+                GetTypeInfo().
+                GetInterfaces().
+                Where(t => t.GetTypeInfo().IsGenericType == true && t.GetGenericTypeDefinition() == typeof(ICollection<>));
+
+            if(collectionTypes.Count() != 1)
+            {
+                return false;
+            }
+
+            Type genericType = collectionTypes.
+                First().
+                GetGenericArguments()[0];
+
+            object collection = _activatorService.CreateInstance(propertyInfo.PropertyType);
+            MethodInfo add = propertyInfo.PropertyType.GetMethod("Add");
+            foreach (string subValue in value.Split(','))
+            {
+                add.Invoke(collection, new[] { Convert.ChangeType(subValue, genericType) });
+            }
+
+            propertyInfo.SetValue(target, collection);
+
+            return true;
+        }
+    }
+}
---
+++ test/CommandLine/CollectionMapperIntegrationTests.cs
@@ -0,0 +1,84 @@
+﻿using JeremyTCD.DotNetCore.Utils;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace JeremyTCD.DotNet.CommandLine.Tests
+{
+    public class CollectionMapperIntegrationTests
+    {
+        [Fact]
+        public void TryMap_ReturnsFalseIfValueIsNull()
+        {
+            // Arrange
+            CollectionMapper collectionMapper = new CollectionMapper(null);
+
+            // Act
+            bool result = collectionMapper.TryMap(null, null, null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TryMap_ReturnsFalseIfPropertyTypeIsNotAssignableToCollection()
+        {
+            // Arrange
+            PropertyInfo propertyInfo = typeof(DummyModel).GetProperty(nameof(DummyModel.NotCollection));
+            CollectionMapper collectionMapper = new CollectionMapper(null);
+
+            // Act
+            bool result = collectionMapper.TryMap(propertyInfo, "dummy", null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TryMap_ReturnsTrueIfPropertyTypeIsBoolAndMappingIsSuccessful()
+        {
+            // Arrange
+            Mock<ILoggingService<ActivatorService>> mockASLS = new Mock<ILoggingService<ActivatorService>>();
+            IActivatorService activatorService = new ActivatorService(mockASLS.Object);
+            PropertyInfo propertyInfo = typeof(DummyModel).GetProperty(nameof(DummyModel.StringCollection));
+            DummyModel dummyModel = new DummyModel();
+            CollectionMapper collectionMapper = new CollectionMapper(activatorService);
+            string dummyString = "1,2,3";
+
+            // Act
+            bool result = collectionMapper.TryMap(propertyInfo, dummyString, dummyModel);
+
+            // Assert
+            Assert.Equal(new List<string> { "1", "2", "3" }, dummyModel.StringCollection);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TryMap_PerformsConversionsWhenMapping()
+        {
+            // Arrange
+            Mock<ILoggingService<ActivatorService>> mockASLS = new Mock<ILoggingService<ActivatorService>>();
+            IActivatorService activatorService = new ActivatorService(mockASLS.Object);
+            PropertyInfo propertyInfo = typeof(DummyModel).GetProperty(nameof(DummyModel.IntCollection));
+            DummyModel dummyModel = new DummyModel();
+            CollectionMapper collectionMapper = new CollectionMapper(activatorService);
+            string dummyString = "1,2,3";
+
+            // Act
+            bool result = collectionMapper.TryMap(propertyInfo, dummyString, dummyModel);
+
+            // Assert
+            Assert.Equal(new List<int> { 1, 2, 3 }, dummyModel.IntCollection);
+            Assert.True(result);
+        }
+
+        private class DummyModel
+        {
+            public List<string> StringCollection { get; set; }
+            public List<int> IntCollection { get; set; }
+            public string NotCollection { get; set; }
+        }
+    }
+}
