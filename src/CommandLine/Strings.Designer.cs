4293b95c52e2c13aa10aaa968a19e9c523c4dddc
JeremyTCD <jeremytanchongdao@gmail.com>
2017-09-05 14:47:57 +0800

    Added strings to Strings.resx

--- src/CommandLine/Strings.Designer.cs
+++ src/CommandLine/Strings.Designer.cs
@@ -23,7 +23,7 @@ namespace JeremyTCD.DotNet.CommandLine {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Strings {
+    public class Strings {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -37,7 +37,7 @@ namespace JeremyTCD.DotNet.CommandLine {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("JeremyTCD.DotNet.CommandLine.Strings", typeof(Strings).GetTypeInfo().Assembly);
@@ -52,7 +52,7 @@ namespace JeremyTCD.DotNet.CommandLine {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -64,19 +64,81 @@ namespace JeremyTCD.DotNet.CommandLine {
         /// <summary>
         ///   Looks up a localized string similar to Invalid arguments.
         /// </summary>
-        internal static string Exception_Arguments {
+        public static string Exception_Arguments {
             get {
                 return ResourceManager.GetString("Exception_Arguments", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Arguments contain multiple commands:
-        ///{0}.
+        ///   Looks up a localized string similar to &quot;{0}&quot; is an invalid value for option &quot;-{1}&quot;.
         /// </summary>
-        internal static string Exception_ArgumentsContainMultipleCommands {
+        public static string Exception_InvalidOptionValue {
             get {
-                return ResourceManager.GetString("Exception_ArgumentsContainMultipleCommands", resourceCulture);
+                return ResourceManager.GetString("Exception_InvalidOptionValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Malformed arguments &quot;{0}&quot;.
+        /// </summary>
+        public static string Exception_MalformedArguments {
+            get {
+                return ResourceManager.GetString("Exception_MalformedArguments", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Option &quot;-{0}&quot; does not exist.
+        /// </summary>
+        public static string Exception_OptionDoesNotExist {
+            get {
+                return ResourceManager.GetString("Exception_OptionDoesNotExist", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Parameter &quot;{0}&quot; should not be null.
+        /// </summary>
+        public static string Exception_ParameterShouldBeNull {
+            get {
+                return ResourceManager.GetString("Exception_ParameterShouldBeNull", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Property &quot;{0}&quot; cannot hold multiple values.
+        /// </summary>
+        public static string Exception_PropertyCannotHoldMultipleValues {
+            get {
+                return ResourceManager.GetString("Exception_PropertyCannotHoldMultipleValues", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Property &quot;{0}&quot; does not have an OptionAttribute.
+        /// </summary>
+        public static string Exception_PropertyDoesNotHaveOptionAttribute {
+            get {
+                return ResourceManager.GetString("Exception_PropertyDoesNotHaveOptionAttribute", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Property &quot;{0}&quot; is not a flag.
+        /// </summary>
+        public static string Exception_PropertyIsNotAFlag {
+            get {
+                return ResourceManager.GetString("Exception_PropertyIsNotAFlag", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Type &quot;{0}&quot; does not have a CommandAttribute.
+        /// </summary>
+        public static string Exception_TypeDoesNotHaveCommandAttribute {
+            get {
+                return ResourceManager.GetString("Exception_TypeDoesNotHaveCommandAttribute", resourceCulture);
             }
         }
     }
