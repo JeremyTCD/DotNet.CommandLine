0d1f6ff9e46eafb08886430db2e7e68396ab4a79
JeremyTCD <jeremytanchongdao@gmail.com>
2017-09-06 18:44:27 +0800

    Added CommandLineServiceCollections

---
+++ src/CommandLine/CommandLineServiceCollections.cs
@@ -0,0 +1,30 @@
+﻿using JeremyTCD.DotNetCore.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace JeremyTCD.DotNet.CommandLine
+{
+    public static class CommandLineServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCommandLine(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.TryAdd(ServiceDescriptor.Singleton<ICommandLineTool, CommandLineTool>());
+            serviceCollection.TryAdd(ServiceDescriptor.Singleton<ICommandSetFactory, CommandSetFactory>());
+            serviceCollection.TryAdd(ServiceDescriptor.Singleton<IParser, Parser>());
+            serviceCollection.TryAdd(ServiceDescriptor.Singleton<IPrinter, Printer>());
+            serviceCollection.TryAdd(ServiceDescriptor.Singleton<ICommandFactory, CommandFactory>());
+            serviceCollection.TryAdd(ServiceDescriptor.Singleton<IOptionFactory, OptionFactory>());
+            serviceCollection.TryAdd(ServiceDescriptor.Singleton<IArgumentsFactory, ArgumentsFactory>());
+            serviceCollection.TryAdd(ServiceDescriptor.Singleton<IModelFactory, ModelFactory>());
+
+            // Multiple implementations of IMapper are allowed
+            serviceCollection.AddSingleton<IMapper, CollectionMapper>();
+            serviceCollection.AddSingleton<IMapper, FlagMapper>();
+            serviceCollection.AddSingleton<IMapper, StringConvertibleMapper>();
+
+            serviceCollection.AddUtils();
+
+            return serviceCollection;
+        }
+    }
+}
---
+++ test/CommandLine/IntegrationTests/CommandLineServiceCollectionExtensionsIntegrationTests.cs
@@ -0,0 +1,52 @@
+﻿using JeremyTCD.DotNetCore.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace JeremyTCD.DotNet.CommandLine.Tests.IntegrationTests
+{
+    public class CommandLineServiceCollectionExtensionsIntegrationTests
+    {
+        [Fact]
+        public void AddCommandLine_ConfiguresServicesCorrectly()
+        {
+            // Arrange 
+            ServiceCollection services = new ServiceCollection();
+
+            // Act
+            services.AddCommandLine();
+
+            // Assert
+            ServiceDescriptorComparer comparer = new ServiceDescriptorComparer();
+            Assert.Contains(ServiceDescriptor.Singleton<ICommandLineTool, CommandLineTool>(), services, comparer);
+            Assert.Contains(ServiceDescriptor.Singleton<ICommandSetFactory, CommandSetFactory>(), services, comparer);
+            Assert.Contains(ServiceDescriptor.Singleton<IParser, Parser>(), services, comparer);
+            Assert.Contains(ServiceDescriptor.Singleton<IPrinter, Printer>(), services, comparer);
+            Assert.Contains(ServiceDescriptor.Singleton<ICommandFactory, CommandFactory>(), services, comparer);
+            Assert.Contains(ServiceDescriptor.Singleton<IOptionFactory, OptionFactory>(), services, comparer);
+            Assert.Contains(ServiceDescriptor.Singleton<IArgumentsFactory, ArgumentsFactory>(), services, comparer);
+            Assert.Contains(ServiceDescriptor.Singleton<IModelFactory, ModelFactory>(), services, comparer);
+            Assert.Contains(ServiceDescriptor.Singleton<IMapper, CollectionMapper>(), services, comparer);
+            Assert.Contains(ServiceDescriptor.Singleton<IMapper, FlagMapper>(), services, comparer);
+            Assert.Contains(ServiceDescriptor.Singleton<IMapper, StringConvertibleMapper>(), services, comparer);
+        }
+
+        [Fact]
+        public void AddCommandLine_ConfiguresMappersSoTheyCanBeInjectedInIEnumerable()
+        {
+            // Arrange 
+            ServiceCollection services = new ServiceCollection();
+
+            // Act
+            services.AddCommandLine();
+            IEnumerable<IMapper> mappers = services.BuildServiceProvider().GetService<IEnumerable<IMapper>>();
+
+            // Assert
+            Assert.Equal(3, mappers.Count());
+            Assert.Contains(mappers, m => m.GetType() == typeof(CollectionMapper));
+            Assert.Contains(mappers, m => m.GetType() == typeof(FlagMapper));
+            Assert.Contains(mappers, m => m.GetType() == typeof(StringConvertibleMapper));
+        }
+    }
+}
