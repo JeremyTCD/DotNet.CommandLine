233c01c346ca18fbce05472a37e9ebad494712fd
JeremyTCD <jeremytanchongdao@gmail.com>
2017-09-08 21:44:05 +0800

    Added AppOptions

---
+++ src/CommandLine/AppOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace JeremyTCD.DotNet.CommandLine
+{
+    public class AppOptions
+    {
+        public string ExecutableName { get; set; }
+        public string FullName { get; set; }
+        public string Version { get; set; }
+    }
+}
