0bcfb697ba9d2f5f1fd45295899ea8795bb9b256
JeremyTCD <jeremytanchongdao@gmail.com>
2017-09-13 21:57:55 +0800

    Added basic template for Command abstract type

---
+++ src/CommandLine/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace JeremyTCD.DotNet.CommandLine
+{
+    public abstract class Command : ICommand
+    {
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+        public abstract bool IsDefault { get; }
+
+        public abstract int Run(ParseResult parseResult, AppContext appContext);
+    }
+}
