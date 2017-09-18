0446854e0d567202255237909d6c2a5b1fdf2827
JeremyTCD <jeremytanchongdao@gmail.com>
2017-09-18 21:39:42 +0800

    Added comments for Command

--- src/CommandLine/Core/Commands/Command.cs
+++ src/CommandLine/Core/Commands/Command.cs
@@ -7,8 +7,15 @@
         public abstract bool IsDefault { get; }
 
         [Option(typeof(Strings), nameof(Strings.OptionShortName_Help), nameof(Strings.OptionLongName_Help), nameof(Strings.OptionDescription_Help))]
-        public bool Help { get; }
+        public bool Help { get; set; }
 
+        /// <summary>
+        /// If <paramref name="parseResult"/> contains a <see cref="ParseException"/> instance, prints exception and a get help tip before returning 0.
+        /// If <see cref="Help"/> is true, prints help and returns 1. Otherwise, calls <see cref="RunCommand(ParseResult, AppContext)"/>.
+        /// </summary>
+        /// <param name="parseResult"></param>
+        /// <param name="appContext"></param>
+        /// <returns></returns>
         public virtual int Run(ParseResult parseResult, AppContext appContext)
         {
             if(parseResult.ParseException != null){
